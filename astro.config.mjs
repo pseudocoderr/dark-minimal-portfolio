1b51f76c6cdb09754392fdf0cd06c64025b14718
pseudocoderr <pseudocoderr@users.noreply.github.com>
2026-09-21 05:50:32 +0000

    Initial commit: DarkMinimal portfolio (Firebase like-button removed)

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://pseudocoderr.github.io",
+  base: "/dark-minimal-portfolio",
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
+  },
+  output: "static",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
+});
